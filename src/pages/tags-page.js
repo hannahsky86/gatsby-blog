cc370141d45e740a4f558bc1d338e17fa5bc647b
Hannah Roach <hannah@siimpl.io>
2022-07-07 10:33:45 -0600

    needs clean up

---
+++ src/pages/tags-page.js
@@ -0,0 +1,13 @@
+import {Link} from "gatsby";
+import React from "@types/react";
+
+const TagsPage = ({ tag_dict }) => (
+    <h1>TEST TAGS</h1>
+    // <div className="tags">
+    //     {Object.keys(tag_dict).map((item, idx) =>
+    //         <Link to={`/tags-page`} key={`${idx}-tag`}>{item}</Link>
+    //     )}
+    // </div>
+)
+
+export default Tags
